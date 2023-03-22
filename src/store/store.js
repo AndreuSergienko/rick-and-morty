5fdf6f79b429550a5d0cf169e1380b5a45879f72
Andrey <andrey.sergienko.dev@gmail.com>
2023-03-22 20:14:05 +0300

    Added reducer for switchers

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,12 @@
 import { combineReducers, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import { characters, spinner } from './reducers';
+import { characters, spinner, switchers } from './reducers';
 
 const reducers = combineReducers({
 	characters,
 	spinner,
+	switchers,
 });
 
 export const store = createStore(reducers, composeWithDevTools());
